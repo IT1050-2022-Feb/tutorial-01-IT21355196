131b377340962cdeea52812d92afd91550101611
K. L. Shashin Kalpajith <99177323+IT21355196@users.noreply.github.com>
2022-02-21 04:41:09 +0000

    Tutorial 01

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,20 @@
 #include <stdio.h>
 
 int main() {
-  
+
+  int mark1,mark2;
+  float average;
+
+  printf("Enter the marks of the subject 01: ");
+  scanf("%d" , &mark1);
+
+  printf("Enter the marks of the subject 02: ");
+  scanf("%d" , &mark2);
+
+  average = (mark1 +mark2)/2.0;
+
+  printf("Average marks of tghe two subjects are %.2f" , average);
+
   return 0;
 }
 

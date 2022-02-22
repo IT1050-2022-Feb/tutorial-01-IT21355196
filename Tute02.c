21ab777c42b1954d00293709010aa344fd12d027
K. L. Shashin Kalpajith <99177323+IT21355196@users.noreply.github.com>
2022-02-22 12:42:57 +0000

    Tutorial 01

--- Tute02.c
+++ Tute02.c
@@ -20,5 +20,23 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 int main() {
   
+  	int dist , amount;
+	
+	printf("Distance -> ");
+	scanf("%d" , &dist);
+	
+	if(dist<=30)
+	{
+		amount = dist * 50;
+		
+		printf("Amount = %d " , amount);
+	}
+	else if(dist>30)
+	{
+		amount = 30 * 50 + (dist-30) * 40 ;
+		
+		printf("Amount = %d " , amount);
+	}
+  
   return 0;
 }
